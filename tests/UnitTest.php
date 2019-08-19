a6966baf732560b2d65520ad8c5bef3d384ccceb
Owen Voke <owzie123@gmail.com>
2019-08-19 15:23:41 +0100

    Add unit test

---
+++ tests/UnitTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+class UnitTest extends BaseTestCase
+{
+    /** @test */
+    public function itIsTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
